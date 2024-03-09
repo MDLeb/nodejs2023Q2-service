db55b70ec15687312bc4458be24766850175d5c5
Mariya <m.lebadina@freeplay.io>
2024-03-09 11:47:24 +0300

    feat: generate crud resources using nest cli

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlbumModule } from './album/album.module';
+import { UserModule } from './user/user.module';
+import { ArtistModule } from './artist/artist.module';
+import { FavoriteModule } from './favorite/favorite.module';
+import { TrackModule } from './track/track.module';
 
 @Module({
-  imports: [],
+  imports: [AlbumModule, UserModule, ArtistModule, FavoriteModule, TrackModule],
   controllers: [AppController],
   providers: [AppService],
 })
